ab095b83116ee0d7bf77b02750c24079b04118f3
Mahima Chatterjee <mahimachatterjee.info@gmail.com>
2026-07-19 17:21:34 +0530

    Update solution for Not Null Constraint

--- not-null-constraint/solution.py
+++ not-null-constraint/solution.py
@@ -1,5 +1,11 @@
-););
-  quantity INTEGER DEFAULT 0,  quantity INTEGER DEFAULT 0,
-CREATE TABLE products (CREATE TABLE products (
   name TEXT NOT NULL DEFAULT 'Unknown',  name TEXT NOT NULL DEFAULT 'Unknown',
   price INTEGER NOT NULL,  price INTEGER NOT NULL,
+  quantity INTEGER DEFAULT 0,  quantity INTEGER DEFAULT 0,
+););
+
+-- Do not modify below this line ---- Do not modify below this line --
+SELECT column_name, data_type, is_nullable, column_defaultSELECT column_name, data_type, is_nullable, column_default
+FROM information_schema.columnsFROM information_schema.columns
+WHERE table_name = 'products';WHERE table_name = 'products';
+
+CREATE TABLE products(CREATE TABLE products(
