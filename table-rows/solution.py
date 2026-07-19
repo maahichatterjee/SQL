d5c95bb78b7d205258a24e33d51e41f451760f17
Mahima Chatterjee <mahimachatterjee.info@gmail.com>
2026-07-19 15:57:58 +0530

    Add solution for Table Rows

---
+++ table-rows/solution.py
@@ -0,0 +1,17 @@
+
+
+-- Do not modify below this line ---- Do not modify below this line --
+INSERT INTO cities (name, population, country, is_capital, founded_date) INSERT INTO cities (name, population, country, is_capital, founded_date) 
+VALUES ('New York', 8175133, 'United States', FALSE, '1624-01-01'),VALUES ('New York', 8175133, 'United States', FALSE, '1624-01-01'),
+
+
+););
+  country TEXT,  country TEXT,
+  is_capital BOOLEAN,  is_capital BOOLEAN,
+  founded_date DATE  founded_date DATE
+        ('Beijing', 21516000, 'China', TRUE, '1045-01-01'),        ('Beijing', 21516000, 'China', TRUE, '1045-01-01'),
+        ('Paris', 2243833, 'France', TRUE, '0259-01-01'),        ('Paris', 2243833, 'France', TRUE, '0259-01-01'),
+        ('London', 8136000, 'United Kingdom', TRUE, '0043-01-01'),        ('London', 8136000, 'United Kingdom', TRUE, '0043-01-01'),
+  population INTEGER,  population INTEGER,
+  name TEXT,  name TEXT,
+CREATE TABLE cities(CREATE TABLE cities(
