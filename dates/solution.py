49d0c2dcdcefdfbb1dddeb47d86736644a674a79
Mahima Chatterjee <mahimachatterjee.info@gmail.com>
2026-08-01 19:38:19 +0530

    Add solution for Dates

---
+++ dates/solution.py
@@ -0,0 +1,16 @@
+CREATE TABLE events(CREATE TABLE events(
+
+
+
+
+
+
+
+-- Do not modify below this line ---- Do not modify below this line --
+INSERT INTO events (id, event_date, event_time, event_timestamp) INSERT INTO events (id, event_date, event_time, event_timestamp) 
+  VALUES (1, '2000-01-01', '12:30:10', '2000-01-01 12:30:10');  VALUES (1, '2000-01-01', '12:30:10', '2000-01-01 12:30:10');
+
+    id INTEGER PRIMARY KEY,    id INTEGER PRIMARY KEY,
+););
+    event_time TIME,    event_time TIME,
+    event_timestamp TIMESTAMP    event_timestamp TIMESTAMP
