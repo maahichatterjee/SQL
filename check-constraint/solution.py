9a8b1233c517c7d8dee312b53c3a7b077f7e60da
Mahima Chatterjee <mahimachatterjee.info@gmail.com>
2026-08-01 16:57:03 +0530

    Update solution for Check Constraint

--- check-constraint/solution.py
+++ check-constraint/solution.py
@@ -1,4 +1,9 @@
 CREATE TABLE products(CREATE TABLE products(
+    id INTEGER PRIMARY KEY,    id INTEGER PRIMARY KEY,
+    name TEXT,    name TEXT,
+    price INTEGER CHECK (price>=0),    price INTEGER CHECK (price>=0),
+    status TEXT CHECK (status IN ('available', 'out of stock'))    status TEXT CHECK (status IN ('available', 'out of stock'))
+););
 
 
 
@@ -6,12 +11,5 @@ CREATE TABLE products(CREATE TABLE products(
 SELECT SELECT 
     c.table_name,    c.table_name,
     c.column_name,     c.column_name, 
-    id INTEGER PRIMARY KEY,    id INTEGER PRIMARY KEY,
-))
-    name TEXT,    name TEXT,
-    price INTEGER CHECK (price>=0),    price INTEGER CHECK (price>=0),
-    status TEXT CHECK (status IN ('available', 'out of stock'))    status TEXT CHECK (status IN ('available', 'out of stock'))
     c.data_type,     c.data_type, 
     CASE     CASE 
-        WHEN pk.column_name IS NOT NULL THEN 'YES'        WHEN pk.column_name IS NOT NULL THEN 'YES'
-        ELSE 'NO'        ELSE 'NO'
