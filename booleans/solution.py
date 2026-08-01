81c934f27ac7febe723ca8e0cefb5a1ad517de63
Mahima Chatterjee <mahimachatterjee.info@gmail.com>
2026-08-01 18:09:15 +0530

    Add solution for Booleans

---
+++ booleans/solution.py
@@ -0,0 +1,17 @@
+CREATE TABLE three_column_table (CREATE TABLE three_column_table (
+
+
+
+
+
+
+-- Do not modify below this line ---- Do not modify below this line --
+INSERT INTO three_column_table (id, is_active, is_admin) INSERT INTO three_column_table (id, is_active, is_admin) 
+  VALUES (1, TRUE, FALSE),  VALUES (1, TRUE, FALSE),
+
+
+
+););
+    id INTEGER PRIMARY KEY ,    id INTEGER PRIMARY KEY ,
+    is_active BOOLEAN,    is_active BOOLEAN,
+    is_admin BOOLEAN    is_admin BOOLEAN
