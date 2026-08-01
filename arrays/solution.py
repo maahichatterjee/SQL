ba892a6bcd659a822ff9a55157742607946c693b
Mahima Chatterjee <mahimachatterjee.info@gmail.com>
2026-08-01 19:45:35 +0530

    Add solution for Arrays

---
+++ arrays/solution.py
@@ -0,0 +1,9 @@
+CREATE TABLE orders(CREATE TABLE orders(
+    id INTEGER PRIMARY KEY,    id INTEGER PRIMARY KEY,
+    items TEXT[],    items TEXT[],
+    total_price INTEGER    total_price INTEGER
+););
+
+
+
+
