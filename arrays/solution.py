8c6a90308abb96d9ff7c4449391a1bb2676fc693
Mahima Chatterjee <mahimachatterjee.info@gmail.com>
2026-08-01 19:45:44 +0530

    Update solution for Arrays

--- arrays/solution.py
+++ arrays/solution.py
@@ -1,9 +1,10 @@
-CREATE TABLE orders(CREATE TABLE orders(
-    id INTEGER PRIMARY KEY,    id INTEGER PRIMARY KEY,
-    items TEXT[],    items TEXT[],
     total_price INTEGER    total_price INTEGER
 ););
 
 
 
 
+
+CREATE TABLE orders(CREATE TABLE orders(
+    id INTEGER PRIMARY KEY,    id INTEGER PRIMARY KEY,
+    items TEXT[],    items TEXT[],
