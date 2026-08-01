3b69e99dc5daad9f409fb989f0c7e8796a5100e1
Mahima Chatterjee <mahimachatterjee.info@gmail.com>
2026-08-01 19:28:09 +0530

    Update solution for Strings

--- strings/solution.py
+++ strings/solution.py
@@ -1,4 +1,9 @@
-CREATE TABLE (operating_systems)(CREATE TABLE (operating_systems)(
+CREATE TABLE operating_systems(CREATE TABLE operating_systems(
+    id INTEGER PRIMARY KEY,    id INTEGER PRIMARY KEY,
+    name VARCHAR(255),    name VARCHAR(255),
+    version CHAR(10),    version CHAR(10),
+    market_share NUMERIC(5,2)    market_share NUMERIC(5,2)
+););
 
 
 
@@ -8,10 +13,3 @@ CREATE TABLE (operating_systems)(CREATE TABLE (operating_systems)(
 
 -- Do not modify below this line ---- Do not modify below this line --
 INSERT INTO operating_systems (id, name, version, market_share) INSERT INTO operating_systems (id, name, version, market_share) 
-VALUESVALUES
-    (1, 'Windows', '10', 75.51),    (1, 'Windows', '10', 75.51),
-    id INTEGER PRIMARY KEY,    id INTEGER PRIMARY KEY,
-););
-    name VARCHAR(255),    name VARCHAR(255),
-    version CHAR(10),    version CHAR(10),
-    market_share NUMERIC(5,2)    market_share NUMERIC(5,2)
