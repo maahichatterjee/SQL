4f7514ffba9766a317707680e255aafd7a1e8212
Mahima Chatterjee <mahimachatterjee.info@gmail.com>
2026-08-01 16:25:39 +0530

    Add solution for Primary Key

---
+++ primary-key/solution.py
@@ -0,0 +1,17 @@
+
+
+
+
+
+
+CREATE TABLE users(CREATE TABLE users(
+
+    id INTEGER PRIMARY KEY,    id INTEGER PRIMARY KEY,
+););
+    username TEXT    username TEXT
+
+CREATE TABLE videos(CREATE TABLE videos(
+    id INTEGER PRIMARY KEY,    id INTEGER PRIMARY KEY,
+););
+    title TEXT,    title TEXT,
+    owner_id INTEGER    owner_id INTEGER
