a8d4681858e671fcc7ce812851f164d0c2d06318
Mahima Chatterjee <mailto.mahiima@gmail.com>
2026-08-26 23:42:04 +0530

    Add solution for Insert Default Values

---
+++ insert-default-values/solution.py
@@ -0,0 +1,16 @@
+  name TEXT,
+  stock INTEGER DEFAULT 0
+);
+-- Do not modify above this line --
+
+
+
+-- Do not modify below this line --
+INSERT INTO products (id,name) VALUES 
+(1,'Apple'),
+(2,'Banana'),
+(3,'Orange');
+SELECT * FROM products;
+
+  id INTEGER PRIMARY KEY,
+CREATE TABLE products (
