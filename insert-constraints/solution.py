4606247e0bf5c11b0a3ba08e51d57cd8ba0ea70e
Mahima Chatterjee <mailto.mahiima@gmail.com>
2026-08-26 23:58:48 +0530

    Add solution for Insert Constraints

---
+++ insert-constraints/solution.py
@@ -0,0 +1,18 @@
+-- Do not modify above this line --
+
+
+INSERT INTO users (id,name, age, status) VALUES
+  (1,'John Doe',20, 'active');
+
+INSERT INTO users (id, name, age, status) VALUES
+  (2, 'Jane Doe', 27, 'pending');
+
+INSERT INTO users (id, name, age, status) VALUES
+  (3, 'John Smith', 28, 'active');
+
+INSERT INTO users (id, name, age, status) VALUES
+  (4, 'Jane Smith', 30, 'inactive');
+
+
+-- Do not modify below this line --
+SELECT * FROM users;
