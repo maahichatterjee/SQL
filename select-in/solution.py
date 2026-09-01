b5914b690e116df27eff61a72c00313b6a61c3e1
Mahima Chatterjee <mailto.mahiima@gmail.com>
2026-09-01 10:15:32 +0530

    Add solution for Select In

---
+++ select-in/solution.py
@@ -0,0 +1,18 @@
+(4, 'New York', 'USA'),
+(5, 'New Delhi', 'India'),
+(6, 'Shanghai', 'China'),
+(7, 'Sao Paulo', 'Brazil'),
+(8, 'Mexico City', 'Mexico'),
+(9, 'Cairo', 'Egypt'),
+(10, 'Mumbai', 'India'),
+(11, 'Beijing', 'China'),
+(12, 'Los Angeles', 'USA'),
+(13, 'Toronto', 'Canada'),
+(14, 'Vancouver', 'Canada');
+-- Do not modify above this line. --
+
+SELECT name FROM cities WHERE country IN ('USA','Canada','Mexico');
+
+
+
+
