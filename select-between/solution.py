759c245e0a18f95af184e67b70dcad45d1d0f7af
Mahima Chatterjee <mailto.mahiima@gmail.com>
2026-09-01 10:27:17 +0530

    Update solution for Select Between

--- select-between/solution.py
+++ select-between/solution.py
@@ -1,18 +1,6 @@
-(10, 'Mumbai', 'India', 20660000),
-(11, 'Beijing', 'China', 21540000),
 (12, 'Los Angeles', 'USA', 3971000),
 (13, 'Chicago', 'USA', 2716000),
 (14, 'Manila', 'Philippines', 13160000),
 (15, 'Moscow', 'Russia', 12610000),
 (16, 'Istanbul', 'Turkey', 15580000),
 (17, 'Kolkata', 'India', 14780000),
-(18, 'Bangkok', 'Thailand', 8305000),
-(19, 'Jakarta', 'Indonesia', 10560000),
-(20, 'Lahore', 'Pakistan', 11120000),
-(21, 'Chengdu', 'China', 14040000);
--- Do not modify above this line. --
-
-SELCT country FROM cities WHERE population_at BETWEEN 1000000 AND 
-
-10000000;
-(9, 'Cairo', 'Egypt', 20010000),
