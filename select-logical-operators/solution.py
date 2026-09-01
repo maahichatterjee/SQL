ec2f8d3a2c10f0d1f68071b514f5369e88288ab1
Mahima Chatterjee <mailto.mahiima@gmail.com>
2026-09-01 10:39:32 +0530

    Update solution for Select Logical Operators

--- select-logical-operators/solution.py
+++ select-logical-operators/solution.py
@@ -11,8 +11,3 @@
 (20, 'Lahore', 'Pakistan', 11120000),
 (21, 'Chengdu', 'China', 14040000);
 -- Do not modify above this line. --
-
-SELECT names FROM cities WHERE (population<1000000 OR 
-population>10000000) AND name!=c%;
-
-
