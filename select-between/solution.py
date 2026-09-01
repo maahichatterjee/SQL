3a73c8c6a2783e37449d05ab983608ef3fef194c
Mahima Chatterjee <mailto.mahiima@gmail.com>
2026-09-01 10:27:36 +0530

    Update solution for Select Between

--- select-between/solution.py
+++ select-between/solution.py
@@ -1,6 +1,19 @@
-(12, 'Los Angeles', 'USA', 3971000),
-(13, 'Chicago', 'USA', 2716000),
-(14, 'Manila', 'Philippines', 13160000),
-(15, 'Moscow', 'Russia', 12610000),
-(16, 'Istanbul', 'Turkey', 15580000),
-(17, 'Kolkata', 'India', 14780000),
+(11, 'Beijing', 'China', 21540000),
+(10, 'Mumbai', 'India', 20660000),
+(9, 'Cairo', 'Egypt', 20010000),
+(8, 'Mexico City', 'Mexico', 8918000),
+(7, 'Sao Paulo', 'Brazil', 12330000),
+(5, 'New Delhi', 'India', 19640000),
+(6, 'Shanghai', 'China', 25700000),
+(4, 'New York', 'USA', 8419000),
+(3, 'Paris', 'France', 2141000),
+(1, 'Tokyo', 'Japan', 37400068),
+(2, 'London', 'England', 8982000),
+INSERT INTO cities (id, name, country, population) VALUES
+
+);
+    population INTEGER
+    country TEXT,
+    name TEXT,
+    id INT PRIMARY KEY,
+CREATE TABLE cities (
