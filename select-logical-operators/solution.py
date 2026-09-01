0fe77f3dacfc0aa2d8cfc81645068c4acec6cfb1
Mahima Chatterjee <mailto.mahiima@gmail.com>
2026-09-01 10:39:54 +0530

    Update solution for Select Logical Operators

--- select-logical-operators/solution.py
+++ select-logical-operators/solution.py
@@ -4,10 +4,3 @@
 (13, 'Chicago', 'USA', 2716000),
 (14, 'Manila', 'Philippines', 13160000),
 (15, 'Moscow', 'Russia', 12610000),
-(16, 'Istanbul', 'Turkey', 15580000),
-(17, 'Kolkata', 'India', 14780000),
-(18, 'Bangkok', 'Thailand', 8305000),
-(19, 'Jakarta', 'Indonesia', 10560000),
-(20, 'Lahore', 'Pakistan', 11120000),
-(21, 'Chengdu', 'China', 14040000);
--- Do not modify above this line. --
